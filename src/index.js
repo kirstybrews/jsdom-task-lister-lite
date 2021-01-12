227fff5515b7c23c5d7809b78b9c3e5b3a357f6d
github username <github email address>
2021-01-12 16:59:09 -0600

    finished w/ delete button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', listTask)
 });
+
+function listTask(event){
+  event.preventDefault()
+  let task = event.target.newTaskDescription.value
+  addList(task)
+}
+
+function addList(task) {
+  let listItem = document.createElement('li')
+  listItem.textContent = task
+  listItem.id = counter
+  // let deleteItem = document.createElement('form')
+  // deleteItem.onsubmit = deleteListItem(listItem.id);
+  // deleteItem.id = 'deleteForm'
+  // deleteItem.addEventListener('submit', (event) => {
+  //   console.log(event)
+  // })
+  let deleteButton = document.createElement('button')
+  // deleteButton.type = 'submit'
+  deleteButton.innerText = 'delete'
+  // deleteItem.appendChild(deleteButton)
+  deleteButton.addEventListener('click', (event) => {
+    console.log(event.target.parentElement)
+    event.target.parentElement.remove()
+  })
+  listItem.appendChild(deleteButton)
+  document.querySelector('#tasks').appendChild(listItem)
+  counter += 1
+}
+
+let counter = 0
+
+// function deleteListItem(id) {
+//   listItem = document.querySelector('#id')
+//   console.log(listItem.value)
+//   document.querySelector('#tasks').removeChild(listItem)
+// }
